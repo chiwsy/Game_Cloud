8f23cd232802a091bbc90aede8072d1c8f592a18
chiwsy <chiwsylove@gmail.com>
2014-09-08 23:25:03 -0400

    controller

--- Assets/PlayerController/PlayerOrbit.cs
+++ Assets/PlayerController/PlayerOrbit.cs
@@ -14,21 +14,31 @@ public class PlayerOrbit : MonoBehaviour {
 	public float height=10.0f;
 	
 	public float vel=0.1f;
+	public float maxvel=0.15f;
 	// Use this for initialization
 	public Vector3 gravity;
 	public float energy;
-	public float theta;
-	public float phi;
+	//public float theta;
+	//public float phi;
+	
+	public Vector3 innerVel=new Vector3(0.0f,0.0f,0.0f);//x for radius direction, y for theta, z for phi
+	public Vector3 innerPos=new Vector3(5010.0f,0.0f,Mathf.PI/2.0f);
+	//force and torque;
+	public float fv=0.05f;
+	public Vector3 friction=new Vector3(2.0f,1.0f,1.0f);
+	public Vector3 restvel=new Vector3(0.0f,0.001f,0.001f);
+	//public Vector3 rv;
 	
 	void Start () {
 		//Vector3 dir=gameObject.transform.position-Ground.transform.position;
 		//gravity=dir.normalized/dir.sqrMagnitude;
 		//energy=10;
-		theta=0;
-		phi=Mathf.PI/2;
+		//theta=0;
+		//phi=Mathf.PI/2;
 		gameObject.transform.position=new Vector3(5010.0f,0.0f,0.0f);
 		PlayerCam.transform.position=new Vector3(5020.0f,0.0f,0.0f);
 		PlayerCam.rigidbody.freezeRotation=true;
+		
 	}
 	
 	void OnCollisionEnter(Collision collision) {
@@ -47,17 +57,59 @@ public class PlayerOrbit : MonoBehaviour {
 		
 		Vector3 prePostion=gameObject.transform.position;
 		
+		if(Input.GetAxisRaw("Vertical")>0){
+			innerVel.x-=10.0f*fv*Time.deltaTime;
+			//gameObject.rigidbody.AddRelativeForce(-fv*gameObject.transform.up);
+		}
+		else if(Input.GetAxisRaw("Vertical")<0){
+			innerVel.x+=10.0f*fv*Time.deltaTime;
+			//gameObject.rigidbody.AddRelativeForce(fv*gameObject.transform.up);
+		}
+		if(Input.GetAxisRaw("Horizontal")>0){
+			//gameObject.rigidbody.AddRelativeForce(-fv*gameObject.transform.right);
+			if(Mathf.Abs(innerVel.z)<1e-6) innerVel.z+=fv*Time.deltaTime;
+			else{
+				float alpha=Mathf.Atan2(innerVel.y,innerVel.z);
+				innerVel.y-=fv*Mathf.Cos(alpha)*Time.deltaTime;
+				innerVel.z+=fv*Mathf.Sin(alpha)*Time.deltaTime;
+			}
+		}
+		else if(Input.GetAxisRaw("Horizontal")<0){
+			//gameObject.rigidbody.AddRelativeForce(fv*gameObject.transform.right);
+			if(Mathf.Abs(innerVel.z)<1e-6) innerVel.z+=fv*Time.deltaTime;
+			else{
+				float alpha=Mathf.Atan2(innerVel.y,innerVel.z);
+				innerVel.y+=fv*Mathf.Cos(alpha)*Time.deltaTime;
+				innerVel.z-=fv*Mathf.Sin(alpha)*Time.deltaTime;
+			}
+		}		
+		//theta+=.2f*Mathf.PI/360.0f*vel;
+		if(Mathf.Abs(innerVel.y)>maxvel) innerVel.y=innerVel.y/Mathf.Abs(innerVel.y)*maxvel;
+		if(Mathf.Abs(innerVel.z)>maxvel) innerVel.z=innerVel.z/Mathf.Abs(innerVel.z)*maxvel;
+		float tmp=restvel.x;
+		restvel=0.1f*new Vector3(0.0f,innerVel.y,innerVel.z).normalized;
+		restvel.x=tmp;
+		//phi+=.3f*Mathf.PI/360.0f*vel;
+		innerPos+=Time.deltaTime*innerVel;
+		//theta=innerPos.y;
+		//phi=innerPos.z;
+		if(innerPos.y>2*Mathf.PI)
+			innerPos.y-=2*Mathf.PI;
+		if(innerPos.y<0)
+			innerPos.y+=2*Mathf.PI;
+		if(innerPos.z>2*Mathf.PI)
+			innerPos.z-=2*Mathf.PI;
+		if(innerPos.z<0)
+			innerPos.z+=2*Mathf.PI;
 		
-		theta+=.2f*Mathf.PI/360.0f*vel;
+		innerVel=new Vector3(Mathf.Lerp(innerVel.x,restvel.x,friction.x*Time.deltaTime),
+		                     Mathf.Lerp(innerVel.y,restvel.y,friction.y*Time.deltaTime),
+		                     Mathf.Lerp(innerVel.z,restvel.z,friction.z*Time.deltaTime));
 		
-		phi+=.3f*Mathf.PI/360.0f*vel;
-		if(theta>2*Mathf.PI)
-			theta-=2*Mathf.PI;
-		if(phi>2*Mathf.PI)
-			phi-=2*Mathf.PI;
-		
-		gameObject.transform.position=Vector3.Scale( new Vector3(5050.0f,5050.0f,5050.0f)
-		         ,new Vector3(Mathf.Sin (phi)*Mathf.Cos(theta),Mathf.Cos (phi),Mathf.Sin (phi)*Mathf.Sin(theta)));
+		gameObject.transform.position=Vector3.Scale( new Vector3(innerPos.x,innerPos.x,innerPos.x)
+		                                            ,new Vector3(Mathf.Sin (innerPos.z)*Mathf.Cos(innerPos.y),
+		                                            Mathf.Cos (innerPos.z),
+		                                            Mathf.Sin (innerPos.z)*Mathf.Sin(innerPos.y)));
 		gameObject.transform.rotation=Quaternion.LookRotation(gameObject.transform.position-prePostion,gameObject.transform.position);
 		Vector3 tarPostion=gameObject.transform.position-2*dist*gameObject.transform.forward+2*height*gameObject.transform.up;
 		//gameObject.transform.
